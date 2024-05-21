c7fcccd562e0380befe47848225ee691d4a1d823
Andres Cabrera <andrescabrera@Accounts-iMac.local>
2024-05-21 12:17:21 -0400

    todos written

---
+++ app/controllers/agent.py
@@ -0,0 +1,3 @@
+#TODO: import_csv(): Function that reads our current system's data and migrate it to our new database. Use pandas.
+
+#TODO: add new info to database on current systems. We need prod for this.
