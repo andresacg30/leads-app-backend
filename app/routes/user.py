7c31e5a74c3583088437411aaf0da89275b61a05
Andres Cabrera <andresacg30@gmail.com>
2024-09-06 17:10:48 -0400

    new login process

---
+++ app/routes/user.py
@@ -0,0 +1,45 @@
+
+from fastapi import Body, APIRouter, HTTPException
+from passlib.context import CryptContext
+
+import app.controllers.user as user_controller
+from app.auth.jwt_handler import sign_jwt
+from app.models.user import UserModel, UserData, UserSignIn
+
+router = APIRouter()
+
+hash_helper = CryptContext(schemes=["bcrypt"])
+
+
+@router.post("/login")
+async def user_login(user_credentials: UserSignIn = Body(...)):
+    try:
+        user_exists = await user_controller.get_user_by_field(email=user_credentials.username)
+    except user_controller.UserNotFoundError:
+        user_exists = None
+    if user_exists:
+        password = hash_helper.verify(user_credentials.password, user_exists["password"])
+        if password:
+            return sign_jwt(user_credentials.username)
+
+        raise HTTPException(status_code=403, detail="Incorrect email or password")
+
+    raise HTTPException(status_code=403, detail="Incorrect email or password")
+
+
+@router.post("", response_model=UserData)
+async def user_signup(user: UserModel = Body(...)):
+    try:
+        user_exists = await user_controller.get_user_by_field(email=user.email)
+    except user_controller.UserNotFoundError:
+        user_exists = None
+    if user_exists:
+        raise HTTPException(
+            status_code=409, detail="user with email supplied already exists"
+        )
+
+    await user_controller.create_user(user)
+    return {
+        "full_name": user.full_name,
+        "email": user.email
+    }
--- settings.py
+++ settings.py
@@ -10,6 +10,12 @@ class Settings(BaseSettings):
     mongodb_url: str = os.environ.get("MONGO_URL")
     environment: str = os.environ.get("ENVIRONMENT", "development")
     mongodb_name: str = os.environ.get("MONGO_DB_NAME")
+    realm_app_id: str = os.environ.get("REALM_APP_ID")
+    jwt_secret_key: str = os.environ.get("JWT_SECRET_KEY")
+    email_address: str = os.environ.get("EMAIL_ADDRESS")
+    email_password: str = os.environ.get("EMAIL_PASSWORD")
+    email_service: str = os.environ.get("EMAIL_SERVICE")
+    email_port: int = os.environ.get("EMAIL_PORT")
     testing: bool = False
 
 
