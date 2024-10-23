c8b3b18329bac2ca0687ecd5501260772adbf8ef
Andres Cabrera <andresacg30@gmail.com>
2024-10-23 15:31:05 -0400

    added email verifiation

--- settings.py
+++ settings.py
@@ -12,14 +12,11 @@ class Settings(BaseSettings):
     mongodb_name: str = os.environ.get("MONGO_DB_NAME")
     realm_app_id: str = os.environ.get("REALM_APP_ID")
     jwt_secret_key: str = os.environ.get("JWT_SECRET_KEY")
-    email_address: str = os.environ.get("EMAIL_ADDRESS")
-    email_password: str = os.environ.get("EMAIL_PASSWORD")
-    email_service: str = os.environ.get("EMAIL_SERVICE")
-    email_port: int = os.environ.get("EMAIL_PORT")
     api_key: str = os.environ.get("API_KEY")
     testing: bool = False
     stripe_api_key: str = os.environ.get("STRIPE_API_KEY")
     frontend_url: str = os.environ.get("FRONTEND_URL")
+    mailgun_api_key: str = os.environ.get("MAILGUN_API_KEY")
 
 
 def get_settings() -> Settings:
