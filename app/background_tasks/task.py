f47a5703d1942ebec9ef05c86b26de49ba7a6fb0
Andres Cabrera <andresacg30@gmail.com>
2024-10-30 12:20:05 -0400

    logs

--- app/background_tasks/task.py
+++ app/background_tasks/task.py
@@ -2,4 +2,6 @@ from app.resources import scheduler
 
 
 def get_scheduled_jobs():
-    return scheduler.get_jobs()
+    jobs = scheduler.get_jobs()
+    jobs_dict = [job for job in jobs]
+    return jobs_dict
