155fcb4d81ab439d471cabd1fb9048b90afa177b
Andres Cabrera <andresacg30@gmail.com>
2024-11-04 16:15:33 -0500

    bla

--- worker.py
+++ worker.py
@@ -1,13 +1,14 @@
+# worker.py
+
 import os
 import threading
 from fastapi import FastAPI
 import uvicorn
 import redis
 from rq import Worker
-from settings import get_redis_settings
+
 
 app = FastAPI()
-settings = get_redis_settings()
 
 
 @app.get("/")
@@ -15,18 +16,19 @@ def health_check():
     return {"status": "OK"}
 
 
-def start_worker():
-    redis_server = settings.redis_api_address
-    redis_port = settings.redis_api_port
-    redis_url = f'redis://{redis_server}:{redis_port}'
-    conn = redis.from_url(redis_url)
-    worker = Worker(['default'], connection=conn)
-    worker.work()
+def start_api():
+    port = int(os.getenv('PORT', 8080))
+    uvicorn.run(app, host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
-    worker_thread = threading.Thread(target=start_worker)
-    worker_thread.start()
+    api_thread = threading.Thread(target=start_api)
+    api_thread.start()
 
-    port = int(os.getenv('PORT', 8080))
-    uvicorn.run(app, host='0.0.0.0', port=port)
+    redis_server = os.getenv('REDIS_API_ADDRESS', 'localhost')
+    redis_port = os.getenv('REDIS_PORT', 6379)
+    redis_password = os.getenv('REDIS_PASSWORD', None)
+    redis_url = f'redis://{redis_server}:{redis_port}'
+    conn = redis.from_url(redis_url, password=redis_password)
+    worker = Worker(['default'], connection=conn)
+    worker.work()
