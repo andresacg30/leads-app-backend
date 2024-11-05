fedac09863249772bc2438f931d7fcfb49e186de
Andres Cabrera <andresacg30@gmail.com>
2024-11-05 11:30:41 -0500

    worker fixes

--- rq_dashboard_app.py
+++ rq_dashboard_app.py
@@ -53,3 +53,8 @@ flask_app = RootPathMiddleware(create_rq_dashboard_app(), '/admin/dashboard')
 
 
 app.mount('/admin/dashboard', WSGIMiddleware(flask_app))
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
