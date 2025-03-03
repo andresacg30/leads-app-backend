a8901971f83fde51582f59e6c33689a9d76bfb2b
Andres Cabrera <andresacg30@gmail.com>
2025-03-03 14:33:47 -0500

    deleted life jacket from og campaign

--- app/tools/constants.py
+++ app/tools/constants.py
@@ -12,7 +12,6 @@ DEFAULT_LEAD_LIMIT = 7
 OG_CAMPAIGNS = [
     "6668b634a88f8e5a8dde197e",  # Capital
     "6668b634a88f8e5a8dde197f",  # Freedom
-    "6668b634a88f8e5a8dde197c",  # LJL
     "6668b634a88f8e5a8dde1980",  # Secure
     "667c4fe1664290677c6dc24d",  # Independant
     "668d8c02e990ca1088f7a3e0",  # Legacy LT
